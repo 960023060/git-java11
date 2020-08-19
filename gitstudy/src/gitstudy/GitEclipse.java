681a8b8b52fca6387212108253706ddfc85b26eb
960023060 <960023060@qq.com>
2020-08-19 16:17:12 +0800

    二ICI

--- gitstudy/src/gitstudy/GitEclipse.java
+++ gitstudy/src/gitstudy/GitEclipse.java
@@ -3,9 +3,10 @@ package gitstudy;
 public class GitEclipse {
 
 public static void main(String[] args) {
-	System.out.println("123456");
-	System.out.println("1111111);
+
+	System.out.println("1111111");
 	System.out.println("2222222");
+	System.out.println("3333333");
 }
 }
 
